9941b62ef274da94c6dd9696c258988a9b967c0c
123name <123@hotmail.com>
2022-08-03 17:00:49 +1200

    implementing express server to handle changes to static state initiated by the front end

---
+++ src/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+/usr/bin/bash session.sh $(pwd)
+
--- src/staticState.js
+++ src/staticState.js
@@ -1,18 +1,53 @@
 //should digest http requests from front end
 //
+const express = require('express');
+const cors = require('cors');
 let zmq = require("zeromq");
 let channel = "channel name";
 
+//express setup
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+//zeroMQ setup
 const push = zmq.socket("pub");
 push.bindSync("tcp://127.0.0.1:3005");
+
+//static state object
+let state = {
+  avg: 1,
+  vol: 2,
+  min: 3, 
+  max: 4
+}
+
+/* only for testing */
+app.get('/setpoint/:num', (req, res) => {
+  console.log(state.test);
+  state.test = req.params.num;
+  console.log(state.test);
+  res.sendStatus(200);
+});
+
+/* for use in production */
+app.post('/update', (req, res) => {
+  state.avg = req.body.avg;
+  state.vol = req.body.vol;
+  state.min = req.body.min;
+  state.max = req.body.max;
+  res.sendStatus(200);
+});
+
+app.listen(4000, () => {
+  console.log("listening on port 4000");
+});
 
 //hardcoded output for testing purposes
 const main = async() => {
   while(true) {
-    let rand = Math.floor((Math.random() * 999) + 1000).toString()
-    let calculated = {
-      maxSomething: rand
-    }
+    let calculated = state;
+    console.log(state);
     push.send(
       [channel, Buffer.from(JSON.stringify(calculated).toString('base64'))])
     await new Promise((resolve) => {setTimeout(resolve, 2000)});
